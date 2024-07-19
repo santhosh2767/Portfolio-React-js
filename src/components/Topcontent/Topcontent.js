8f4f2410ecb66100bf11ad743962779f3f851b3a
Santhosh Kumar S <123973189+santhosh2767@users.noreply.github.com>
2024-07-19 21:46:03 +0530

    Update Topcontent.js

--- src/components/Topcontent/Topcontent.js
+++ src/components/Topcontent/Topcontent.js
@@ -7,7 +7,7 @@ const Topcontent = () => {
         <div className='Topcontent_container'> 
         <h1> Santhosh kumar </h1>
         <p> A professional Frontend developer</p>
-        <a href='https://drive.google.com/file/d/1mRmu9oYdkxJ54LkUh9rHzRcVohWMIxYd/view?usp=sharing' target='blank'> <button className='Topcontent_db'> Download Cv</button></a>
+        <a href="https://drive.google.com/file/d/1mRmu9oYdkxJ54LkUh9rHzRcVohWMIxYd/view?usp=sharing" target='blank'> <button className='Topcontent_db'> Download Cv</button></a>
         <a href='https://github.com/santhosh2767' target='_blank' rel='noopener noreferrer'>
         <button className='Topcontent_mw'>My Work</button>
         </a>
